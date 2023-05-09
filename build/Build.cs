c167bd0bd2a78c436af3e36a2aff0b3b275df25f
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-05-09 17:44:00 +0300

    refactor build

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Linq;
+using CliWrap;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
@@ -20,21 +13,46 @@ class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Docker);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] Solution Solution { get; set; }
+    AbsolutePath DockerFiles { get; set; }
+
+    protected override void OnBuildInitialized()
+    {
+        base.OnBuildInitialized();
+        DockerFiles = Solution.Directory / ".." / "build" / "DockerFiles";
+    }
 
     Target Restore => _ => _
         .Executes(() => DotNetRestore(setting => setting.SetProjectFile(Solution.Path)));
 
     Target Clean => _ => _
         .DependsOn(Restore)
-        .Executes(() => DotNetClean(setting => setting.SetProject(Solution.Path)));
+        .Executes(() => DotNetClean(setting => setting.SetProject(Solution.Path)
+            .SetConfiguration(Configuration)));
 
     Target Compile => _ => _
         .DependsOn(Clean)
-        .Executes(() => DotNetBuild(settings => settings.SetProjectFile(Solution.Path)));
+        .Executes(() => DotNetBuild(settings => settings.SetProjectFile(Solution.Path)
+            .SetConfiguration(Configuration)));
+
+    Target Tests => _ => _
+        .DependsOn(Compile)
+        .Executes(() => DotNetTest(settings => settings.SetProjectFile(Solution.Path)
+            .SetConfiguration(Configuration)));
+
+    Target Docker => _ => _
+        .DependsOn(Tests)
+        .Executes(() => Cli.Wrap("docker-compose")
+            .WithWorkingDirectory(DockerFiles)
+            .WithArguments("-f ./PostresSQL.yml up -d")
+            .WithStandardErrorPipe(PipeTarget.ToDelegate(Log.Error))
+            .WithStandardOutputPipe(PipeTarget.ToDelegate(Log.Information))
+            .ExecuteAsync()
+            .GetAwaiter()
+            .GetResult());
 }
